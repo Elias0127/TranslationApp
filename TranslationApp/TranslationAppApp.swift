3e7316d68dd2467a7f132798cfb067386f47e1e9
Elias0127 <eliasmekonen001@gmail.com>
2024-04-05 21:18:24 -0700

    Initial Commit

---
+++ TranslationApp/TranslationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationAppApp.swift
+//  TranslationApp
+//
+//  Created by Elias Woldie on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
